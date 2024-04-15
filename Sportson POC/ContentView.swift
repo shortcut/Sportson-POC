a8d67163c06adeef1816fd903420f6e4ce9af25e
MarcoG <marco.giustozzi@shortcut.no>
2024-04-15 10:10:48 +0200

    Initial Commit

---
+++ Sportson POC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sportson POC
+//
+//  Created by Marco-Shortcut on 15. 4. 2024..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sportson POC/Sportson_POCApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sportson_POCApp.swift
+//  Sportson POC
+//
+//  Created by Marco-Shortcut on 15. 4. 2024..
+//
+
+import SwiftUI
+
+@main
+struct Sportson_POCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
